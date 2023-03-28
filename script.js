c37d71713dee61c2535434e13c3128ef444c0b4a
Rajat kumar <30478219+Rajat9927@users.noreply.github.com>
2023-03-28 13:00:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-// complete the given function
-
-function palindrome(str){
-
+function palindrome(str) {
+    str = str.toLowerCase();
+    // Using Spread operator on string
+    let arr = [...str];
+    // console.log(arr);
+    let arrCopy = [...str];    // as reverse method also make changes to original array
+    let arrReverse = arr.reverse();
+        arr = arrCopy;
+    console.log(`reverseArray:${arrReverse}`);
+    console.log(`array:${arr}`);
+ 
+     for (let i = 0; i < arr.length; i++) {
+        if (arrCopy[i] != arrReverse[i]) { 
+            return false;
+        }
+    }
+    return true;
 }
+ 
+console.log(palindrome("abc"))
 module.exports = palindrome
